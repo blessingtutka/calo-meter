2bc190bec70c40760916d77895bdc29ea752a65e
blessingtutka <blessingtutka298@gmail.com>
2025-08-02 14:45:31 +0200

    refactor(layout): add ui, theme to main layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,60 @@
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { UserProvider } from '@/providers/userProvider';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useEffect } from 'react';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import 'react-native-reanimated';
+import '../assets/styles/global.css';
+
+SplashScreen.preventAutoHideAsync();
+
+const TransparentDarkTheme = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        background: 'transparent',
+    },
+};
+
+const CustomDefaultTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        placeholder: '#808080',
+        text: DefaultTheme.colors.text,
+    },
+};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? TransparentDarkTheme : CustomDefaultTheme}>
+            <UserProvider>
+                <Stack>
+                    <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+                    <Stack.Screen name='(pages)' options={{ headerShown: false }} />
+                    <Stack.Screen name='+not-found' />
+                </Stack>
+                <StatusBar style='auto' />
+            </UserProvider>
+        </ThemeProvider>
+    );
 }
