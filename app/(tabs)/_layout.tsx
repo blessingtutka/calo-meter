97cbe15b48a4d3a1759bef68e02ff498eadadfeb
blessingtutka <blessingtutka298@gmail.com>
2025-08-02 14:44:03 +0200

    feat(tabs): add dashboard tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,37 +9,45 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+                tabBarButton: HapticTab,
+                tabBarBackground: TabBarBackground,
+                tabBarStyle: Platform.select({
+                    ios: {
+                        // Use a transparent background on iOS to show the blur effect
+                        position: 'absolute',
+                    },
+                    default: {},
+                }),
+            }}
+        >
+            <Tabs.Screen
+                name='dashboard'
+                options={{
+                    title: 'Dashboard',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name='house.fill' color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name='explore'
+                options={{
+                    title: 'Explore',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name='paperplane.fill' color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name='auth'
+                options={{
+                    title: 'Auth',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name='person.3.fill' color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
