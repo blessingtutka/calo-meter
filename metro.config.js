0db87fbeb63a3bb8be10f670d661cf04a9c78ae6
blessingtutka <blessingtutka298@gmail.com>
2025-08-19 21:02:52 +0200

    chore(config): update metro config

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,10 @@ const { withNativeWind } = require('nativewind/metro');
 const { withLibsodiumResolver } = require('@burnt-labs/abstraxion-react-native/metro.libsodium');
 
 const config = getDefaultConfig(__dirname);
+
 config.resolver.unstable_enablePackageExports = false;
+
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
 const xionConfig = withLibsodiumResolver(config);
 module.exports = withNativeWind(xionConfig, { input: './assets/styles/global.css' });
